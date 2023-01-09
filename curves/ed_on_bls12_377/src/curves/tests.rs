3fbd69ea823546d1f2400fd2eda4ce14af51a7bb
Achim Schneider <achim@parity.io>
2023-01-09 13:01:43 +0100

    fix tests ed_on_bls12_377

--- curves/ed_on_bls12_377/src/curves/tests.rs
+++ curves/ed_on_bls12_377/src/curves/tests.rs
@@ -1,4 +1,19 @@
-use crate::*;
+use crate::HostFunctions;
 use ark_algebra_test_templates::*;
+use ark_std::vec::Vec;
 
-test_group!(te; EdwardsProjective; te);
+pub struct Host {}
+
+impl HostFunctions for Host {
+    fn ed_on_bls12_377_mul_affine(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
+        sp_io::crypto::ed_on_bls12_377_mul_affine(base, scalar)
+    }
+    fn ed_on_bls12_377_mul_projective(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
+        sp_io::crypto::ed_on_bls12_377_mul_projective(base, scalar)
+    }
+    fn ed_on_bls12_377_msm(bases: Vec<Vec<u8>>, scalars: Vec<Vec<u8>>) -> Vec<u8> {
+        sp_io::crypto::ed_on_bls12_377_msm(bases, scalars)
+    }
+}
+
+test_group!(te; crate::EdwardsProjective<super::Host>; te);
