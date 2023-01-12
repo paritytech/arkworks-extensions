02cee73d0a98a8b7d5a1da70a242ca58c9228679
Achim Schneider <achim@parity.io>
2023-01-12 17:07:10 +0100

    fix p_power_endomorphism foir g2, tests pass!

--- curves/bls12_381/src/curves/tests/mod.rs
+++ curves/bls12_381/src/curves/tests/mod.rs
@@ -1,11 +1,13 @@
-use crate::curves::*;
 use ark_algebra_test_templates::*;
 use ark_ff::{fields::Field, One, UniformRand, Zero};
 use ark_models::{AffineRepr, CurveGroup, Group};
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress, Validate};
 use ark_std::{rand::Rng, test_rng, vec, vec::Vec};
 
-use crate::{Fq, Fr, G1Affine as G1Affine_Host, G1Projective as G1Projective_Host, HostFunctions};
+use crate::{
+    Fq, Fq2, Fr, G1Affine as G1Affine_Host, G1Projective as G1Projective_Host,
+    G2Affine as G2Affine_Host, G2Projective as G2Projective_Host, HostFunctions,
+};
 
 pub struct Host {}
 
@@ -37,12 +39,14 @@ impl HostFunctions for Host {
 }
 
 test_group!(g1; crate::G1Projective<super::Host>; sw);
-// test_group!(g2; crate::G2Projective<super::Host>; sw);
+test_group!(g2; crate::G2Projective<super::Host>; sw);
 test_group!(pairing_output; PairingOutput<crate::Bls12_381<super::Host>>; msm);
 test_pairing!(ark_pairing; crate::Bls12_381<super::Host>);
 
 type G1Projective = G1Projective_Host<Host>;
 type G1Affine = G1Affine_Host<Host>;
+type G2Projective = G2Projective_Host<Host>;
+type G2Affine = G2Affine_Host<Host>;
 
 #[test]
 fn test_g1_endomorphism_beta() {
@@ -72,28 +76,28 @@ fn test_g1_subgroup_non_membership_via_endomorphism() {
     }
 }
 
-// #[test]
-// fn test_g2_subgroup_membership_via_endomorphism() {
-//     let mut rng = test_rng();
-//     let generator = G2Projective::rand(&mut rng).into_affine();
-//     assert!(generator.is_in_correct_subgroup_assuming_on_curve());
-// }
+#[test]
+fn test_g2_subgroup_membership_via_endomorphism() {
+    let mut rng = test_rng();
+    let generator = G2Projective::rand(&mut rng).into_affine();
+    assert!(generator.is_in_correct_subgroup_assuming_on_curve());
+}
 
-// #[test]
-// fn test_g2_subgroup_non_membership_via_endomorphism() {
-//     let mut rng = test_rng();
-//     loop {
-//         let x = Fq2::rand(&mut rng);
-//         let greatest = rng.gen();
+#[test]
+fn test_g2_subgroup_non_membership_via_endomorphism() {
+    let mut rng = test_rng();
+    loop {
+        let x = Fq2::rand(&mut rng);
+        let greatest = rng.gen();
 
-//         if let Some(p) = G2Affine::get_point_from_x_unchecked(x, greatest) {
-//             if !<ark_models::short_weierstrass::Projective<crate::g2::Config::<Host>> as ark_std::Zero>::is_zero(&p.mul_bigint(Fr::characteristic())) {
-//                 assert!(!p.is_in_correct_subgroup_assuming_on_curve());
-//                 return;
-//             }
-//         }
-//     }
-// }
+        if let Some(p) = G2Affine::get_point_from_x_unchecked(x, greatest) {
+            if !<ark_models::short_weierstrass::Projective<crate::g2::Config::<Host>> as ark_std::Zero>::is_zero(&p.mul_bigint(Fr::characteristic())) {
+                assert!(!p.is_in_correct_subgroup_assuming_on_curve());
+                return;
+            }
+        }
+    }
+}
 
 // Test vectors and macro adapted from https://github.com/zkcrypto/bls12_381/blob/e224ad4ea1babfc582ccd751c2bf128611d10936/src/tests/mod.rs
 macro_rules! test_vectors {
@@ -139,14 +143,14 @@ fn g1_uncompressed_valid_test_vectors() {
     test_vectors!(G1Projective, G1Affine, Compress::No, bytes);
 }
 
-// #[test]
-// fn g2_compressed_valid_test_vectors() {
-//     let bytes: &'static [u8] = include_bytes!("g2_compressed_valid_test_vectors.dat");
-//     test_vectors!(G2Projective, G2Affine, Compress::Yes, bytes);
-// }
+#[test]
+fn g2_compressed_valid_test_vectors() {
+    let bytes: &'static [u8] = include_bytes!("g2_compressed_valid_test_vectors.dat");
+    test_vectors!(G2Projective, G2Affine, Compress::Yes, bytes);
+}
 
-// #[test]
-// fn g2_uncompressed_valid_test_vectors() {
-//     let bytes: &'static [u8] = include_bytes!("g2_uncompressed_valid_test_vectors.dat");
-//     test_vectors!(G2Projective, G2Affine, Compress::No, bytes);
-// }
+#[test]
+fn g2_uncompressed_valid_test_vectors() {
+    let bytes: &'static [u8] = include_bytes!("g2_uncompressed_valid_test_vectors.dat");
+    test_vectors!(G2Projective, G2Affine, Compress::No, bytes);
+}
