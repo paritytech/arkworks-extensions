7e8352e039e6205a0ab908cc5366f333ec7294d5
Achim Schneider <achim@parity.io>
2023-01-13 08:48:55 +0100

    BLS12_381 g1 tests

--- curves/bls12_381/src/curves/g1.rs
+++ curves/bls12_381/src/curves/g1.rs
@@ -234,32 +234,62 @@ pub fn endomorphism<T: HostFunctions>(p: &Affine<Config<T>>) -> Affine<Config<T>
     res
 }
 
-// #[cfg(test)]
-// mod test {
+#[cfg(test)]
+mod test {
 
-//     use super::*;
-//     use ark_std::{rand::Rng, UniformRand};
+    use super::*;
+    use crate::HostFunctions;
+    use ark_std::{rand::Rng, UniformRand};
 
-//     fn sample_unchecked() -> Affine<g1::Config> {
-//         let mut rng = ark_std::test_rng();
-//         loop {
-//             let x = Fq::rand(&mut rng);
-//             let greatest = rng.gen();
+    fn sample_unchecked() -> Affine<g1::Config<Host>> {
+        let mut rng = ark_std::test_rng();
+        loop {
+            let x = Fq::rand(&mut rng);
+            let greatest = rng.gen();
 
-//             if let Some(p) = Affine::get_point_from_x_unchecked(x, greatest) {
-//                 return p;
-//             }
-//         }
-//     }
+            if let Some(p) = Affine::get_point_from_x_unchecked(x, greatest) {
+                return p;
+            }
+        }
+    }
 
-//     #[test]
-//     fn test_cofactor_clearing() {
-//         const SAMPLES: usize = 100;
-//         for _ in 0..SAMPLES {
-//             let p: Affine<g1::Config> = sample_unchecked();
-//             let p = p.clear_cofactor();
-//             assert!(p.is_on_curve());
-//             assert!(p.is_in_correct_subgroup_assuming_on_curve());
-//         }
-//     }
-// }
+    pub struct Host {}
+
+    impl HostFunctions for Host {
+        fn bls12_381_multi_miller_loop(a: Vec<Vec<u8>>, b: Vec<Vec<u8>>) -> Vec<u8> {
+            sp_io::crypto::bls12_381_multi_miller_loop(a, b)
+        }
+        fn bls12_381_final_exponentiation(f12: &[u8]) -> Vec<u8> {
+            sp_io::crypto::bls12_381_final_exponentiation(f12)
+        }
+        fn bls12_381_msm_g1(bases: Vec<Vec<u8>>, bigints: Vec<Vec<u8>>) -> Vec<u8> {
+            sp_io::crypto::bls12_381_msm_g1(bases, bigints)
+        }
+        fn bls12_381_mul_projective_g1(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
+            sp_io::crypto::bls12_381_mul_projective_g1(base, scalar)
+        }
+        fn bls12_381_mul_affine_g1(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
+            sp_io::crypto::bls12_381_mul_affine_g1(base, scalar)
+        }
+        fn bls12_381_msm_g2(bases: Vec<Vec<u8>>, bigints: Vec<Vec<u8>>) -> Vec<u8> {
+            sp_io::crypto::bls12_381_msm_g2(bases, bigints)
+        }
+        fn bls12_381_mul_projective_g2(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
+            sp_io::crypto::bls12_381_mul_projective_g2(base, scalar)
+        }
+        fn bls12_381_mul_affine_g2(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
+            sp_io::crypto::bls12_381_mul_affine_g2(base, scalar)
+        }
+    }
+
+    #[test]
+    fn test_cofactor_clearing() {
+        const SAMPLES: usize = 100;
+        for _ in 0..SAMPLES {
+            let p: Affine<g1::Config<Host>> = sample_unchecked();
+            let p = p.clear_cofactor();
+            assert!(p.is_on_curve());
+            assert!(p.is_in_correct_subgroup_assuming_on_curve());
+        }
+    }
+}
