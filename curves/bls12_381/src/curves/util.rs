0e75ecafba717fd49477db6f6074d070109c4c8a
Achim Schneider <achim@parity.io>
2023-01-28 15:46:57 +0100

    fix imports

--- curves/bls12_381/src/curves/util.rs
+++ curves/bls12_381/src/curves/util.rs
@@ -3,11 +3,9 @@ use ark_serialize::SerializationError;
 use ark_std::vec::Vec;
 use sp_ark_models::{short_weierstrass::Affine, AffineRepr};
 
+use crate::g2::g2::Config as G2Config;
 use crate::HostFunctions;
-use ark_bls12_381::{
-    fq, fq::Fq, fq2::Fq2, g1::Config as G1Config, g2::Config as G2Config, G1Affine, G2Affine,
-};
-
+use ark_bls12_381::{fq, fq::Fq, fq2::Fq2, g1::Config as G1Config, G1Affine, G2Affine};
 pub const G1_SERIALIZED_SIZE: usize = 48;
 pub const G2_SERIALIZED_SIZE: usize = 96;
 
