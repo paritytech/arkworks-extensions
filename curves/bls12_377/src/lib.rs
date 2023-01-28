4bd2f36f9366f869d293bd38c83a94293ddd9b88
Achim Schneider <achim@parity.io>
2023-01-28 13:31:02 +0100

    bls12_377, export field elements

--- curves/bls12_377/src/lib.rs
+++ curves/bls12_377/src/lib.rs
@@ -32,6 +32,6 @@ mod curves;
 pub mod constraints;
 
 #[cfg(feature = "curve")]
-pub use ark_bls12_377::{Fq, Fq12Config, Fq2, Fq2Config, Fq6Config, Fr, FrConfig};
+pub use ark_bls12_377::{Fq, Fq12Config, Fq2, Fq2Config, Fq6Config, Fr, FrConfig, fq12, fr, fq2};
 #[cfg(feature = "curve")]
 pub use curves::*;
