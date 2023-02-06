0a1797d49ce0651a7c6c6e70b7c33c392367f0e6
Davide Galassi <davxy@datawok.net>
2023-02-06 19:48:40 +0100

    Refactory of ed-on-bls12-377

--- curves/ed_on_bls12_377/src/curves/mod.rs
+++ curves/ed_on_bls12_377/src/curves/mod.rs
@@ -1,13 +1,15 @@
-use ark_ff::MontFp;
-use ark_serialize::{CanonicalDeserialize, Compress, Validate};
-use ark_std::{io::Cursor, marker::PhantomData, vec::Vec};
-use sp_ark_models::{
+use ark_ec::{
     twisted_edwards::{Affine, MontCurveConfig, Projective, TECurveConfig},
     CurveConfig,
 };
+use ark_ed_on_bls12_377::EdwardsConfig as OrgEdConfig;
+use ark_ff::MontFp;
+use ark_serialize::{CanonicalDeserialize, Compress, Validate};
+use ark_std::{io::Cursor, marker::PhantomData, vec::Vec};
+
 use sp_ark_utils::serialize_argument;
 
-use crate::{fq::Fq, fr::Fr};
+use crate::fq::Fq;
 
 #[cfg(test)]
 mod tests;
@@ -19,35 +21,41 @@ pub type EdwardsProjective<H> = Projective<EdwardsConfig<H>>;
 pub struct EdwardsConfig<H: HostFunctions>(PhantomData<fn() -> H>);
 
 pub trait HostFunctions: 'static {
-    fn ed_on_bls12_377_mul_affine(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8>;
-    fn ed_on_bls12_377_mul_projective(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8>;
     fn ed_on_bls12_377_msm(bases: Vec<Vec<u8>>, scalars: Vec<Vec<u8>>) -> Vec<u8>;
+    fn ed_on_bls12_377_mul_projective(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8>;
+    fn ed_on_bls12_377_mul_affine(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8>;
 }
 
 impl<H: HostFunctions> CurveConfig for EdwardsConfig<H> {
-    type BaseField = Fq;
-    type ScalarField = Fr;
+    type BaseField = <OrgEdConfig as CurveConfig>::BaseField;
+    type ScalarField = <OrgEdConfig as CurveConfig>::ScalarField;
 
-    /// COFACTOR = 4
-    const COFACTOR: &'static [u64] = &[4];
-
-    /// COFACTOR_INV =
-    /// 527778859339273151515551558673846658209717731602102048798421311598680340096
-    const COFACTOR_INV: Fr =
-        MontFp!("527778859339273151515551558673846658209717731602102048798421311598680340096");
+    const COFACTOR: &'static [u64] = <OrgEdConfig as CurveConfig>::COFACTOR;
+    const COFACTOR_INV: Self::ScalarField = <OrgEdConfig as CurveConfig>::COFACTOR_INV;
 }
 
-impl<H: HostFunctions> TECurveConfig for EdwardsConfig<H> {
-    /// COEFF_A = -1
-    const COEFF_A: Fq = MontFp!("-1");
+/// TODO: peek from upstream once (and if) this is merged:
+/// https://github.com/arkworks-rs/curves/pull/150
+/// GENERATOR_X =
+/// 4497879464030519973909970603271755437257548612157028181994697785683032656389,
+const GENERATOR_X: Fq =
+    MontFp!("4497879464030519973909970603271755437257548612157028181994697785683032656389");
 
-    /// COEFF_D = 3021
-    const COEFF_D: Fq = MontFp!("3021");
+/// TODO: peek from upstream once (and if) this is merged:
+/// https://github.com/arkworks-rs/curves/pull/150
+/// GENERATOR_Y =
+/// 4357141146396347889246900916607623952598927460421559113092863576544024487809
+const GENERATOR_Y: Fq =
+    MontFp!("4357141146396347889246900916607623952598927460421559113092863576544024487809");
+
+impl<H: HostFunctions> TECurveConfig for EdwardsConfig<H> {
+    const COEFF_A: Self::BaseField = <OrgEdConfig as TECurveConfig>::COEFF_A;
+    const COEFF_D: Self::BaseField = <OrgEdConfig as TECurveConfig>::COEFF_D;
 
     /// Generated randomly
     const GENERATOR: EdwardsAffine<H> = EdwardsAffine::<H>::new_unchecked(GENERATOR_X, GENERATOR_Y);
 
-    type MontCurveConfig = EdwardsConfig<H>;
+    type MontCurveConfig = Self;
 
     /// Multiplication by `a` is just negation.
     /// Is `a` 1 or -1?
@@ -97,25 +105,8 @@ impl<H: HostFunctions> TECurveConfig for EdwardsConfig<H> {
 }
 
 impl<H: HostFunctions> MontCurveConfig for EdwardsConfig<H> {
-    /// COEFF_A = 0x8D26E3FADA9010A26949031ECE3971B93952AD84D4753DDEDB748DA37E8F552
-    ///         = 3990301581132929505568273333084066329187552697088022219156688740916631500114
-    const COEFF_A: Fq =
-        MontFp!("3990301581132929505568273333084066329187552697088022219156688740916631500114");
+    const COEFF_A: Self::BaseField = <OrgEdConfig as MontCurveConfig>::COEFF_A;
+    const COEFF_B: Self::BaseField = <OrgEdConfig as MontCurveConfig>::COEFF_B;
 
-    /// COEFF_B = 0x9D8F71EEC83A44C3A1FBCEC6F5418E5C6154C2682B8AC231C5A3725C8170AAD
-    ///         = 4454160168295440918680551605697480202188346638066041608778544715000777738925
-    const COEFF_B: Fq =
-        MontFp!("4454160168295440918680551605697480202188346638066041608778544715000777738925");
-
-    type TECurveConfig = EdwardsConfig<H>;
+    type TECurveConfig = Self;
 }
-
-/// GENERATOR_X =
-/// 4497879464030519973909970603271755437257548612157028181994697785683032656389,
-const GENERATOR_X: Fq =
-    MontFp!("4497879464030519973909970603271755437257548612157028181994697785683032656389");
-
-/// GENERATOR_Y =
-/// 4357141146396347889246900916607623952598927460421559113092863576544024487809
-const GENERATOR_Y: Fq =
-    MontFp!("4357141146396347889246900916607623952598927460421559113092863576544024487809");
