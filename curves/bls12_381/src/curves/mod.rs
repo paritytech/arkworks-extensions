ebf79866e1c6fa8a97d72c95d7b0da3e58a62834
Achim Schneider <achim@parity.io>
2023-02-08 15:46:57 +0100

    fix

--- curves/bls12_381/src/curves/mod.rs
+++ curves/bls12_381/src/curves/mod.rs
@@ -56,6 +56,9 @@ impl<H: HostFunctions> Bls12Config for Config<H> {
             .map(|elem| {
                 let elem: <Bls12<Self> as Pairing>::G1Prepared = elem.into();
                 // let result = serialize_argument(elem.clone());
+                // let mut serialized_result = vec![0u8; result.serialized_size(Compress::Yes)];
+                // let mut cursor = Cursor::new(&mut serialized_result[..]);
+                // result.serialize_compressed(&mut cursor).unwrap();
                 let mut result = vec![0u8; elem.serialized_size(Compress::Yes)];
                 let mut cursor = Cursor::new(&mut result[..]);
                 elem.serialize_compressed(&mut cursor).unwrap();
