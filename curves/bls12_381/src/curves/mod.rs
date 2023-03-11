3fe7116ffac6c7f583eb9b8753516ed925dd5551
Achim Schneider <achim@parity.io>
2023-03-11 09:38:13 +0100

    fix bugs

--- curves/bls12_381/src/curves/mod.rs
+++ curves/bls12_381/src/curves/mod.rs
@@ -63,7 +63,7 @@ impl<H: HostFunctions> Bls12Config for Config<H> {
 
         let result = H::bls12_381_multi_miller_loop(a, b);
 
-        let result = deserialize_result::<Fp12<Fp12Config>>(&result);
+        let result = deserialize_result::<Fp12<Self::Fp12Config>>(&result);
         MillerLoopOutput(result)
     }
 
