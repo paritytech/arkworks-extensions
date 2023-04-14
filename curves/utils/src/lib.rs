4993e3d81273f00f447b5d2c5787646041682d1f
Achim Schneider <achim@parity.io>
2023-04-14 09:36:56 +0200

    iter_ark_to_ark_bytes

--- curves/utils/src/lib.rs
+++ curves/utils/src/lib.rs
@@ -15,3 +15,40 @@ pub fn deserialize_result<Field: CanonicalDeserialize>(result: &Vec<u8>) -> Fiel
     let cursor = Cursor::new(result);
     Field::deserialize_with_mode(cursor, Compress::No, Validate::No).unwrap()
 }
+
+pub fn iter_ark_to_ark_bytes<T, B, I>(iter: I, usage: Usage) -> Result<Vec<u8>, SerializationError>
+where
+    T: CanonicalSerialize,
+    B: Borrow<T>,
+    I: IntoIterator<Item = B>,
+{
+    const LL: usize = 8;
+    let mut iter = iter.into_iter();
+    let len = iter.size_hint().0;
+    let first = iter.next();
+    let mut vec = if let Some(ref e) = first {
+        let size = e.borrow().serialized_size(is_compressed(usage));
+        Vec::with_capacity(LL + size * (1 + len))
+    } else {
+        Vec::with_capacity(LL)
+    };
+    vec.extend_from_slice(&[0u8; LL]);
+    if let Some(e) = first {
+        e.borrow()
+            .serialize_with_mode(&mut vec, is_compressed(usage))?;
+        let mut l = 1;
+        for e in iter {
+            e.borrow()
+                .serialize_with_mode(&mut vec, is_compressed(usage))?;
+            l += 1;
+        }
+        debug_assert_eq!(
+            l, len,
+            "Iterator::size_hint underestimate would slow down release execution."
+        );
+        // let err = |_| scale_error_to_ark_error(scale::Error::from("Arkworks cannot serialize more than 2^32 items."));
+        // let l = u32::try_from(l).map_err(err) ?;
+        (&mut vec)[0..LL].copy_from_slice(&(l as u64).to_le_bytes());
+    }
+    Ok(vec)
+}
