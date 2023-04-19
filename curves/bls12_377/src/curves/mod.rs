40dfc37fd365df7f9b853a484c008b51918f1b88
Achim Schneider <achim@parity.io>
2023-04-19 13:24:59 +0200

    fix types

--- curves/bls12_377/src/curves/mod.rs
+++ curves/bls12_377/src/curves/mod.rs
@@ -52,14 +52,14 @@ impl<H: HostFunctions> Bls12Config for Config<H> {
         a: impl IntoIterator<Item = impl Into<G1Prepared<Self>>>,
         b: impl IntoIterator<Item = impl Into<G2Prepared<Self>>>,
     ) -> MillerLoopOutput<Bls12<Self>> {
-        let a: ArkScale<Vec<<Curve as Pairing>::G1Affine>> = a
+        let a: ArkScale<Vec<<Curve as Pairing>::G1Prepared>> = a
             .into_iter()
             .map(|elem| {
                 let elem: <Bls12<Self> as Pairing>::G1Prepared = elem.into();
             })
             .collect::<Vec<_>>()
             .into();
-        let b: ArkScale<Vec<<Curve as Pairing>::G2Affine>> = b
+        let b: ArkScale<Vec<<Curve as Pairing>::G2Prepared>> = b
             .into_iter()
             .map(|elem| {
                 let elem: <Bls12<Self> as Pairing>::G2Prepared = elem.into();
