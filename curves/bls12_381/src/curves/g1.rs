d193211586a2a9cd7c896693b42b1d0065748ed5
Achim Schneider <achim@parity.io>
2023-04-20 18:16:43 +0200

    clippy

--- curves/bls12_381/src/curves/g1.rs
+++ curves/bls12_381/src/curves/g1.rs
@@ -203,15 +203,16 @@ mod test {
 
     use super::*;
     use crate::HostFunctions;
+    use ark_bls12_381::fq;
     use ark_std::{rand::Rng, UniformRand};
 
     pub struct Host {}
 
     impl HostFunctions for Host {
-        fn bls12_381_multi_miller_loop(a: Vec<u8>, b: Vec<u8>) -> Vec<u8> {
+        fn bls12_381_multi_miller_loop(a: Vec<u8>, b: Vec<u8>) -> Result<Vec<u8>, ()> {
             sp_io::elliptic_curves::bls12_381_multi_miller_loop(a, b)
         }
-        fn bls12_381_final_exponentiation(f12: Vec<u8>) -> Vec<u8> {
+        fn bls12_381_final_exponentiation(f12: Vec<u8>) -> Result<Vec<u8>, ()> {
             sp_io::elliptic_curves::bls12_381_final_exponentiation(f12)
         }
         fn bls12_381_msm_g1(bases: Vec<u8>, bigints: Vec<u8>) -> Result<Vec<u8>, ()> {
