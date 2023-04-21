d3419d6d3bf70a0b103a9972674f4bf092a63946
Achim Schneider <achim@parity.io>
2023-04-21 18:37:17 +0200

    bandersnatch tests

--- curves/ed_on_bls12_381_bandersnatch/src/curves/tests.rs
+++ curves/ed_on_bls12_381_bandersnatch/src/curves/tests.rs
@@ -1,4 +1,30 @@
 use crate::*;
 use ark_algebra_test_templates::*;
 
-test_group!(te; EdwardsProjective; te);
+pub struct Host {}
+
+impl HostFunctions for Host {
+    fn ed_on_bls12_381_bandersnatch_te_msm(
+        bases: Vec<u8>,
+        scalars: Vec<u8>,
+    ) -> Result<Vec<u8>, ()> {
+        sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_te_msm(bases, scalars)
+    }
+    fn ed_on_bls12_381_sw_msm(bases: Vec<u8>, scalars: Vec<u8>) -> Result<Vec<u8>, ()> {
+        sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_sw_msm(bases, scalars)
+    }
+    fn ed_on_bls12_381_bandersnatch_te_mul_projective(
+        base: Vec<u8>,
+        scalar: Vec<u8>,
+    ) -> Result<Vec<u8>, ()> {
+        sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_te_mul_projective(base, scalar)
+    }
+    fn ed_on_bls12_381_bandersnatch_sw_mul_projective(
+        base: Vec<u8>,
+        scalar: Vec<u8>,
+    ) -> Result<Vec<u8>, ()> {
+        sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_sw_mul_projective(base, scalar)
+    }
+}
+
+test_group!(te; EdwardsProjective<Host>; te);
