9d220d5ef5482cee35db39382b26b7a4189ec1d6
Achim Schneider <achim@parity.io>
2023-04-21 19:45:59 +0200

    fix tests

--- curves/ed_on_bls12_381_bandersnatch/src/curves/tests.rs
+++ curves/ed_on_bls12_381_bandersnatch/src/curves/tests.rs
@@ -10,7 +10,10 @@ impl HostFunctions for Host {
     ) -> Result<Vec<u8>, ()> {
         sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_te_msm(bases, scalars)
     }
-    fn ed_on_bls12_381_sw_msm(bases: Vec<u8>, scalars: Vec<u8>) -> Result<Vec<u8>, ()> {
+    fn ed_on_bls12_381_bandersnatch_sw_msm(
+        bases: Vec<u8>,
+        scalars: Vec<u8>,
+    ) -> Result<Vec<u8>, ()> {
         sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_sw_msm(bases, scalars)
     }
     fn ed_on_bls12_381_bandersnatch_te_mul_projective(
