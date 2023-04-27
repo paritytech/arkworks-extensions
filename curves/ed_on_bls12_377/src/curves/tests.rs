d3a6cf3eae8bbf16b5518e5518b98eee3d0e465f
Achim Schneider <achim@parity.io>
2023-04-27 15:53:41 +0200

    refactor ed_on_bls12_377

--- curves/ed_on_bls12_377/src/curves/tests.rs
+++ curves/ed_on_bls12_377/src/curves/tests.rs
@@ -1,17 +1,4 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-use crate::HostFunctions;
 use ark_algebra_test_templates::*;
-use ark_std::vec::Vec;
 
-pub struct Host {}
-
-impl HostFunctions for Host {
-    fn ed_on_bls12_377_msm(bases: Vec<u8>, scalars: Vec<u8>) -> Result<Vec<u8>, ()> {
-        sp_io::elliptic_curves::ed_on_bls12_377_msm(bases, scalars)
-    }
-    fn ed_on_bls12_377_mul_projective(base: Vec<u8>, scalar: Vec<u8>) -> Result<Vec<u8>, ()> {
-        sp_io::elliptic_curves::ed_on_bls12_377_mul_projective(base, scalar)
-    }
-}
-
-test_group!(te; crate::EdwardsProjective<super::Host>; te);
+test_group!(te; crate::EdwardsProjective; te);
