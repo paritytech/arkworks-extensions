02e826531510b72106fcf0167e40198abfd7e787
alvicsam <alvicsam@gmail.com>
2025-03-14 08:46:29 +0100

    ci: add publish dry-run job

--- scripts/crates_publish_manual.sh
+++ scripts/crates_publish_manual.sh
@@ -8,9 +8,20 @@ then
       exit 1
 fi
 
+echo "Publishing ark-models-ext crate"
 cargo publish -p ark-models-ext
+
+echo "Publishing ark-bls12-377-ext crate"
 cargo publish -p ark-bls12-377-ext
+
+echo "Publishing ark-ed-on-bls12-377-ext crate"
 cargo publish -p ark-ed-on-bls12-377-ext
+
+echo "Publishing ark-bls12-381-ext crate"
 cargo publish -p ark-bls12-381-ext
+
+echo "Publishing ark-bw6-761-ext crate"
 cargo publish -p ark-bw6-761-ext
+
+echo "Publishing ark-ed-on-bls12-381-bandersnatch-ext crate"
 cargo publish -p ark-ed-on-bls12-381-bandersnatch-ext
