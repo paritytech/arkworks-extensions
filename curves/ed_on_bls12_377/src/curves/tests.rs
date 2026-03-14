0ba12c8d01474fe72e9ef74915ed7a4f5a9685a8
Davide Galassi <davxy@datawok.net>
2026-03-14 12:01:04 +0100

    ed-on-bls12-377 default hooks

--- curves/ed_on_bls12_377/src/curves/tests.rs
+++ curves/ed_on_bls12_377/src/curves/tests.rs
@@ -1,23 +1,9 @@
 use crate::CurveHooks;
 
 use ark_algebra_test_templates::*;
-use ark_ed_on_bls12_377::EdwardsConfig as ArkConfig;
-use ark_models_ext::CurveConfig;
 
-struct TestHooks;
+impl CurveHooks for () {}
 
-type Config = crate::EdwardsConfig<TestHooks>;
-type Affine = crate::EdwardsAffine<TestHooks>;
-type Projective = crate::EdwardsProjective<TestHooks>;
-
-impl CurveHooks for TestHooks {
-    fn msm(bases: &[Affine], scalars: &[<Config as CurveConfig>::ScalarField]) -> Projective {
-        test_utils::msm_te_generic::<Config, ArkConfig>(bases, scalars)
-    }
-
-    fn mul_projective(base: &Projective, scalar: &[u64]) -> Projective {
-        test_utils::mul_projective_te_generic::<Config, ArkConfig>(base, scalar)
-    }
-}
+type Projective = crate::EdwardsProjective<()>;
 
 test_group!(te; Projective; te);
